d2c89d82b8c3ffb61881bea66b6224ad0c0fc1d3
kkon999 <gon910910@gmail.com>
2020-04-14 22:34:33 +0900

    Java-백준 코딩 입출력,사칙연산

--- src/backjun/Chapter01.java
+++ src/backjun/Chapter01.java
@@ -2,6 +2,8 @@ package backjun;
 
 public class Chapter01 {
 	public static void main(String[] args) {
+		
+		// print 메소드를 사용하여 Hello World 문자형 데이터 출력
 		System.out.println("Hello World");
 	}
 
--- src/backjun/Chapter04.java
+++ src/backjun/Chapter04.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class Chapter04 {
 	public static void main(String[] args) {
 		
+		// Scanner 메소드를 사용하여 데이터를 사용자로부터 입력 받아 인스턴스 sc에 저장
 		Scanner sc = new Scanner(System.in);
+		// int형 변수 A,B를 생성
 		int A , B;
+		// 입출력 메소드 nextInt를 사용하여 데이터를 입력받고 A,B 변수에 저장
 		A = sc.nextInt();
 		B = sc.nextInt();
+		
+		// Print 메소드를 사용하여 변수 A,B에 저장된 두 데이터를 덧셈 연산
 		System.out.println(A+B);
 	}
 
