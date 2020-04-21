a097dd5155c059e2b226e933ac857922b38a7e31
kkon999 <gon910910@gmail.com>
2020-04-21 23:58:52 +0900

    java-단어 개수 세기

---
+++ src/backjun/Chapter09.java
@@ -0,0 +1,19 @@
+package backjun;
+
+import java.util.Scanner; 
+public class Chapter09 {
+public static void main(String[] args) { 
+Scanner scanner = new Scanner(System.in); 
+String text = scanner.nextLine();
+String[] splitWords = text.split(" ");
+int count = 0;
+for (String str : splitWords) {
+if (!str.isEmpty()) { 
+++count; 
+} 
+} 
+System.out.println(count); 
+scanner.close(); 
+} 
+}
+
