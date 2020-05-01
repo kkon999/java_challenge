27c18fd4d00b81b62579f9554c09e2c15a1e502e
kkon999 <gon910910@gmail.com>
2020-05-01 23:51:26 +0900

    Java-입출력

---
+++ src/backjun/Chapter04_02.java
@@ -0,0 +1,20 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class Chapter04_02 {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+				
+		while(s.hasNext()) {
+			int a = s.nextInt();
+			int b = s.nextInt();
+			
+			System.out.println(a+b);
+		}
+		
+		
+	}
+}
+	
