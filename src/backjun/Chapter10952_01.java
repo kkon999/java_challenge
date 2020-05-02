543abb55353cd4f86ef032f9a4863ce4c4edb2f5
kkon999 <gon910910@gmail.com>
2020-05-02 23:35:56 +0900

    Java-10952번:A+B -5

---
+++ src/backjun/Chapter10952_01.java
@@ -0,0 +1,25 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class Chapter10952_01 {
+
+	public static void main(String[] args) {
+		
+		Scanner in=new Scanner(System.in);
+		
+		while(true){
+		
+			int A=in.nextInt();
+			int B=in.nextInt();
+		
+			if(A==0 && B==0){
+				in.close();
+				break;
+			}
+			System.out.println(A+B);
+		}
+		
+	}
+		
+}
