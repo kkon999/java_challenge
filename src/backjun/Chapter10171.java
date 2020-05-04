bfadb19409607bb1e2784cc60a5ce3a8b979a913
kkon999 <gon910910@gmail.com>
2020-05-04 23:42:44 +0900

    java-10171번:입출력

---
+++ src/backjun/Chapter10171.java
@@ -0,0 +1,11 @@
+package backjun;
+
+public class Chapter10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\" );
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
