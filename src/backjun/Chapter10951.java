51fe9ea8ba7c4e26621deae76b8d598c0dac7e71
kkon999 <gon910910@gmail.com>
2020-05-04 23:36:46 +0900

    Java-10951번:A+B -4

--- src/backjun/Chapter10951.java
+++ src/backjun/Chapter10951.java
@@ -2,7 +2,7 @@ package backjun;
 
 import java.util.Scanner;
 
-public class Chapter04_02 {
+public class Chapter10951 {
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
